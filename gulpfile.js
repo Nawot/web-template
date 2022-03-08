8fe071cb2c56dbefd8708e387856efbe0699e0f7
Nawot <Nawot001@gmail.com>
2022-03-08 21:52:19 +0300

    Settings path in gulpfile.js and set global vars

--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,9 @@
-let dist_folder = 'dist'
-let src_folder  = 'src'
+import gulp from 'gulp'
 
-let path        =
-{
-    build: 
-    {
-        html:  dist_folder+'/',
-        pug:   dist_folder+'/',
-        php:   dist_folder+'/',
-        css:   dist_folder+'/css/',
-        js:    dist_folder+'/js/',
-        img:   dist_folder+'/img/',
-        fonts: dist_folder+'/fonts/'
-    },
+import {path} from './gulp/config/path.js'
 
-    src: 
-    {
-        html:  [src_folder+'/html/**/*.html', '!'+src_folder+'/html/**/_*.html'],
-        pug:   [src_folder+'/pug/**/*.pug', '!'+src_folder+'/pug/**/_*.pug'],
-        php:   [src_folder+'/php/**/*.php', '!'+src_folder+'/php/**/_*.php'],
-        css:   [src_folder+'/scss/**/*.scss', '!'+src_folder+'/scss/**/_*.scss'],
-        js:    [src_folder+'/js/**/*.js', '!'+src_folder+'/js/**/_*.js'],
-        img:    src_folder+'/img/**/*.{png,jpg,svg}',
-        fonts:  src_folder+'/fonts/**/*.*'
-
-    },
-
-    watch: 
-    {
-        html:  src_folder+'/html/**/*.html',
-        pug:   src_folder+'/pug/**/*.pug',
-        php:   src_folder+'/php/**/*.php',
-        css:   src_folder+'/scss/**/*.scss',
-        js:    src_folder+'/js/**/*.js',
-        img:   src_folder+'/img/**/*.{png,jpg,svg}',
-        fonts: src_folder+'/fonts/**/*.*'
-    },
-    clean: './'+dist_folder+'/**'
-}
+global.path = path
+global.gulp = gulp
 
 
 let {src, dest}   = require('gulp'),
@@ -67,7 +33,7 @@ function browserUpdate()
         {
             port: 8000,
             keepalive: true,
-            base: `./${dist_folder}/`
+            base: path.root,
         }, function ()
         {
             browsersync.init(
@@ -84,7 +50,7 @@ function browserUpdate()
         {
             server:
             {
-                baseDir: `./${dist_folder}/`,
+                baseDir: path.root,
             },
             port: 8000,
             notify: true
