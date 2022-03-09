cb82eb5e89e5404d69c8737de87339ff27489743
Nawot <Nawot001@gmail.com>
2022-03-09 11:31:41 +0300

    ./

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 import gulp from 'gulp'
 
 import {path} from './gulp/config/path.js'
-import {plugins} from '.gulp/config/plugins.js'
+import {plugins} from './gulp/config/plugins.js'
 
 global.path = path
 global.gulp = gulp
