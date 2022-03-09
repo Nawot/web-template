0be74aa86ed5068605f0841a8f53f25942121705
Nawot <Nawot001@gmail.com>
2022-03-09 13:29:41 +0300

    Update watchForFiles

--- gulpfile.js
+++ gulpfile.js
@@ -13,12 +13,12 @@ import * as tasks from './gulp/tasks/index.js'
 
 function watchForFiles()
 {
-    gulp.watch([path.watch.html], html)
-    gulp.watch([path.watch.pug], pug)
-    gulp.watch([path.watch.php], php)
-    gulp.watch([path.watch.js], js)
-    gulp.watch([path.watch.css], css)
-    gulp.watch([path.watch.img], img)
+    gulp.watch(path.watch.html, tasks.html)
+    gulp.watch(path.watch.pug, tasks.pug)
+    gulp.watch(path.watch.php, tasks.php)
+    gulp.watch(path.watch.js, tasks.js)
+    gulp.watch(path.watch.css, tasks.css)
+    gulp.watch(path.watch.img, tasks.img)
 }
 
 let build = gulp.series(tasks.clean, gulp.parallel(tasks.html, tasks.pug, tasks.php, tasks.js, tasks.css, tasks.img, tasks.fonts))
