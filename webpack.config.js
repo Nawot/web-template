561d071ca0735f0a56e3284ce054256099ac0587
Nawot <nawot001@gmail.com>
2022-06-04 19:04:16 +0000

    Added babel. Solve #2

--- webpack.config.js
+++ webpack.config.js
@@ -26,6 +26,11 @@ export default
         rules:
         [
             {
+                test: /\.js$/,
+                use: ['babel-loader'],
+                exclude: '/node_modules/',
+            },
+            {
                 test: /\.css$/,
                 use: ['style-loader', 'css-loader'],
             },
