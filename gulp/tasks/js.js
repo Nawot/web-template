a1664fb46fcbde2a645c476c04a9101a10aa3f4a
Nawot <nawot001@gmail.com>
2022-06-05 15:21:39 +0000

    Remove fileinclude from js task

--- gulp/tasks/js.js
+++ gulp/tasks/js.js
@@ -11,17 +11,12 @@ export function js()
 {
     const replacequotes = plugins.replacequotes
     const browsersync = plugins.browsersync
-    const fileinclude = plugins.fileinclude
     const gulpif = plugins.gulpif
 
     // Gulp stream
     const noWebpackChain = lazypipe()
 
     return gulp.src(path.src.js, {sourcemap: true})
-    .pipe(fileinclude(
-    {
-        indent: true
-    }))
         .pipe(replacequotes())
         .pipe(gulpif(useWebpack,
             gulpWebpack(webpackConfig, webpack)
