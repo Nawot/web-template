f410d19b0fdfc21d1be64490d15599d69680b1a3
Nawot <nawot001@gmail.com>
2022-06-07 08:55:45 +0000

    Clean. Remove swiper

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,4 @@
-import Swiper from 'swiper'
-
 document.addEventListener('DOMContentLoaded', function()
 {
     // code
-    const swiper = new Swiper('.swiper',
-    {
-        spaceBetween: 20,
-    })
 })
